9481411d65bee4cee5ae04effcbe9f3268ebaacb
Roni Palva-aho <roni.palva-aho@protacon.com>
2020-05-10 17:00:02 +0300

    Secret example

---
+++ src/secrets.example.h
@@ -0,0 +1,11 @@
+#define SECRET_SSID "yourwifissid"
+#define SECRET_PWD "yourwifipass"
+#define SECRET_BROKER "something-ats.iot.your-region.amazonaws.com"
+#define SECRET_TOPIC "my/topic"
+#define SECRET_ID "my-id"
+
+const char SECRET_CERTIFICATE[] = R"(
+-----BEGIN CERTIFICATE-----
+Here is your certificate
+-----END CERTIFICATE-----
+)";
